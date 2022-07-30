c4726884181d0642adf2d1d47c497c0be42d8dd0
Akhil Reddy <102137740+Akhil-4789@users.noreply.github.com>
2022-07-30 09:18:05 +0530

    Update test_mux.py

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -1,10 +1,66 @@
+
 # See LICENSE.vyoma for details
+# Code your testbench here
+#import cocotb
+#from cocotb.triggers import Timer
+#from cocotb.result import TestFailure
+#import random
 
 import cocotb
 from cocotb.triggers import Timer
+from cocotb.result import TestFailure
+import random
 
 @cocotb.test()
-async def test_mux(dut):
-    """Test for mux2"""
-
-    cocotb.log.info('##### CTB: Develop your test here ########')
+def mux_test1(dut):                        #---------------------TEST - 1-------------------------#
+    yield Timer(2)
+    SEL=30
+    INP30=1
+    dut.inp30.value = INP30
+    dut.sel.value = SEL
+    yield Timer(2)
+    if dut.out != INP30:
+         raise TestFailure("MUX failed with: input %s and sel %s actual out was %s but got %s" % (dut.inp30, dut.sel,dut.inp30,dut.out))
+    else:
+         dut.log.info("MUX pased with: input %s and sel %s actual out was %s and got %s" %  (dut.inp30, dut.sel,dut.inp30,dut.out))
+        
+        
+@cocotb.test()        
+def mux_test2(dut):                          #---------------------TEST - 2-------------------------#
+    yield Timer(2)
+    SEL=12
+    INP12=0
+    dut.inp12.value = INP12
+    dut.sel.value = SEL
+    yield Timer(2)
+    if dut.out != INP12:
+        raise TestFailure("MUX failed with: input %s and sel %s actual out was %s but got %s" % (dut.inp12, dut.sel,dut.inp12,dut.out))
+    else:
+        dut.log.info("MUX pased with: input %s and sel %s actual out was %s and got %s" % (dut.inp12, dut.sel,dut.inp12,dut.out))
+             
+        
+@cocotb.test()        
+def mux_test3(dut):                          #---------------------TEST - 3-------------------------#
+    yield Timer(2) 
+    SEL=21
+    INP21=1
+    dut.inp21.value = INP21
+    dut.sel.value = SEL
+    yield Timer(2)
+    if dut.out != INP21:
+        raise TestFailure("MUX failed with: input %s and sel %s actual out was %s but got %s" % (dut.inp21, dut.sel,dut.inp21,dut.out))
+    else:
+        dut.log.info("MUX pased with: input %s and sel %s actual out was %s and got %s" % (dut.inp21, dut.sel,dut.inp21,dut.out))
+             
+@cocotb.test()        
+def mux_test4(dut):                            #---------------------TEST - 4-------------------------#
+    yield Timer(2)
+    SEL=10
+    INP10=1
+    dut.inp10.value = INP10
+    dut.sel.value = SEL
+    yield Timer(2)
+    if dut.out != INP10:
+        raise TestFailure("MUX failed with: input %s and sel %s actual out was %s but got %s" % (dut.inp10, dut.sel,dut.inp10,dut.out))
+    else:
+        dut.log.info("MUX pased with: input %s and sel %s actual out was %s and got %s" % (dut.inp10, dut.sel,dut.inp10,dut.out))
