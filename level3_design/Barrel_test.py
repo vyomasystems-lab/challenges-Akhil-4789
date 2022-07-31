ea785a666bb747881581f4da9462722e0e6e0185
Akhil Reddy <102137740+Akhil-4789@users.noreply.github.com>
2022-07-31 09:47:30 +0530

    Create Barrel_test.py

---
+++ level3_design/Barrel_test.py
@@ -0,0 +1,12 @@
+# See LICENSE.vyoma for details
+
+# SPDX-License-Identifier: CC0-1.0
+
+import os
+import random
+from pathlib import Path
+
+import cocotb
+from cocotb.result import TestFailure
+from cocotb.clock import Clock
+from cocotb.triggers import RisingEdge, FallingEdge
