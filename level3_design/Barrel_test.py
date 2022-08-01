4cfed4cdafe69a9bd87ea00708b7d57395da606e
Akhil Reddy <102137740+Akhil-4789@users.noreply.github.com>
2022-08-01 22:47:55 +0530

    Update Barrel_test.py

--- level3_design/Barrel_test.py
+++ level3_design/Barrel_test.py
@@ -10,3 +10,17 @@ import cocotb
 from cocotb.result import TestFailure
 from cocotb.clock import Clock
 from cocotb.triggers import RisingEdge, FallingEdge
+
+@cocotb.test()
+def Barrel_test1(dut):                      
+    yield Timer(2)
+    c=4
+    d=16
+    dut.dinp.value = d
+    dut.cinp.value = c
+    yield Timer(2)
+    if dut.out != d:
+         raise TestFailure("barrel Shifter failed with test %s and selection %s",(dut.dinp , dut.cinp ))
+    else:
+         dut.log.info("barrel Shifter Passed with test %s ",(dut.dinp))
+        
